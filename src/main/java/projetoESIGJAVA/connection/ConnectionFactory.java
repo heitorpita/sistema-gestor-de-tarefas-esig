6e11d0701af0feb06925214dc98931a1c43a5e11
Heitor <heitor@tom.tom>
2025-08-19 22:55:19 -0300

    feat: add Connection Factory

---
+++ src/main/java/projetoESIGJAVA/connection/ConnectionFactory.java
@@ -0,0 +1,24 @@
+package projetoESIGJAVA.connection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnectionFactory {
+
+    private static EntityManagerFactory factory = null;
+
+    static {
+        if (factory == null) {
+            factory = Persistence.createEntityManagerFactory("tarefas");
+        }
+    }
+
+    public static EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+
+    public static Object getPrimaryKey(Object entity) {
+        return factory.getPersistenceUnitUtil().getIdentifier(entity);
+    }
+}
